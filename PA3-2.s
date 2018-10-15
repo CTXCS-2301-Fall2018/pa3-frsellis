4c41924d0419d7c136436cdba7dcb6248e5ddd48
Frank Ellis <frank.ellis@ctx.edu>
2018-10-15 12:15:09 -0500

    you get no peanuts!

--- PA3-2.s
+++ PA3-2.s
@@ -34,25 +34,20 @@ main:
     @Your modifications will begin at this point
 
     CMP R1, #1       @Check for "peanuts"
-    BEQ _peanut      @If user entered 1 goto _peanuts
+    MOVEQ R3, #75    @If user entered 1 add 75 to R3
     CMP R1, #2       @Check for "chocolate"
-    BEQ _choc        @If user entered 2 goto _choc
+    MOVEQ R3, #125   @If user entered 2 add 125 to R3
     CMP R1, #3       @Check for "pretzels"
-    BEQ _pretzel     @If user entered 3 goto _pretzel
-    LDR R0, =msg7    @If we get here user entered
+    MOVEQ R3, #90   @If user entered 3 add 90 to R3
+    CMP R1, #3
+
+    LDRGE R0, =msg7    @If we get here user entered
                      @an illegal selection so print
                      @error message and terminate
     BL printf
     MOV R7, #1
     SWI #0           @Terminate, error condition
-_peanut:
-    MOV R3, #75      @Move 75 cents into R3
-    BAL _compute
-_choc:
-    MOV R3, #125     @Move 125 cents into R3
-    BAL _compute
-_pretzel:
-    MOV R3, #90      @Move 90 cents into R3
+
 _compute:
     LDR R4, =quantity @Get address of var quantity
     LDR R4, [R4]      @Value of quantity now in R4
